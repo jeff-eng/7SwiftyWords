1255413cdd713dce86e37becd8e7148d979faff3
Jeff Eng <jeff@jeffeng.me>
2016-07-19 12:19:27 -0700

    created user interface in storyboard and added outlets in ViewController

--- Project8/Project8/ViewController.swift
+++ Project8/Project8/ViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var cluesLabel: UILabel!
+    @IBOutlet weak var answersLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var currentAnswer: UITextField!
 
+    @IBAction func submitTapped(sender: AnyObject) {
+    }
+    
+    @IBAction func clearTapped(sender: AnyObject) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
